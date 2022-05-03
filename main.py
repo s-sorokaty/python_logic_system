ae8b57f87ca96c49eaf97e90ace3b9f31f78f2c9
Semyon <sorokaty@rebels.ai>
2022-05-03 22:17:42 +0700

    logic system complite

---
+++ main.py
@@ -0,0 +1,337 @@
+links = {
+    "film_year": {
+        1: "60-70",
+        2: "70-80",
+        3: "90-00",
+        4: "00-10",
+        5: "10-20"
+    },
+    "film_genre": {
+        1: "ужасы",
+        2: "боевик",
+        3: "любовь",
+        4: "комедия",
+        5: "мультфильмы"
+    },
+    "your_mood": {
+        1: "хорошее настроение",
+        2: "спокойное настроение",
+        3: "грустное настроение",
+    },
+    "your_years_old": {
+        1: "от 12 до 18",
+        2: "после 18+"
+    },
+    "films": {
+
+        1: {
+            1: ['Женщина - демон'],
+            2: ['Кроваво - красное'],
+            3: ['Кошмар на улице вязов'],
+            4: ['Звонок'],
+            5: ['Звонок 2'],
+        },
+        2: {
+            1: ['Шаровая молния'],
+            2: ['Человек-паук'],
+            3: ['Робокоп'],
+            4: ['Отчаянный'],
+            5: ['Бэтмен: Начало'],
+        },
+        3: {
+            1: ['Три плюс два'],
+            2: ['Москва слезам не верит'],
+            3: ['И повториться всё...'],
+            4: ['Предсказание'],
+            5: ['Обещание'],
+        },
+        4: {
+            1: ['Фантомас'],
+            2: ['Зорро'],
+            3: ['Назад в будущее'],
+            4: ['Майор Пэйн'],
+            5: ['Жмурки'],
+        },
+        5: {
+            1: ['Вовка в тридевятом царстве'],
+            2: ['Кошки-мышки'],
+            3: ['Ну погоди!'],
+            4: ['История игрушек'],
+            5: ['Тразан'],
+        },
+        6: {
+            1: ['Лучше не стоит'],
+            2: ['Лучше не стоит'],
+            3: ['Лучше не стоит'],
+            4: ['Лучше не стоит'],
+            5: ['Лучше не стоит'],
+
+        }
+    }
+}
+choises = {
+    1: {
+        1: {
+            1: {
+                1: links["films"][6][1],
+                2: links["films"][6][2],
+                3: links["films"][6][3],
+                4: links["films"][6][4],
+                5: links["films"][6][5],
+            },
+            2: {
+                1: links["films"][2][1],
+                2: links["films"][2][2],
+                3: links["films"][2][3],
+                4: links["films"][2][4],
+                5: links["films"][2][5],
+            },
+            3: {
+                1: links["films"][3][1],
+                2: links["films"][3][2],
+                3: links["films"][3][3],
+                4: links["films"][3][4],
+                5: links["films"][3][5],
+            },
+            4: {
+                1: links["films"][4][1],
+                2: links["films"][4][2],
+                3: links["films"][4][3],
+                4: links["films"][4][4],
+                5: links["films"][4][5],
+            },
+            5:{
+                1: links["films"][5][1],
+                2: links["films"][5][2],
+                3: links["films"][5][3],
+                4: links["films"][5][4],
+                5: links["films"][5][5],
+            },
+        },
+        2: {
+            1: {
+                1: links["films"][6][1],
+                2: links["films"][6][2],
+                3: links["films"][6][3],
+                4: links["films"][6][4],
+                5: links["films"][6][5],
+            },
+            2: {
+                1: links["films"][2][1],
+                2: links["films"][2][2],
+                3: links["films"][2][3],
+                4: links["films"][2][4],
+                5: links["films"][2][5],
+            },
+            3: {
+                1: links["films"][3][1],
+                2: links["films"][3][2],
+                3: links["films"][3][3],
+                4: links["films"][3][4],
+                5: links["films"][3][5],
+            },
+            4: {
+                1: links["films"][4][1],
+                2: links["films"][4][2],
+                3: links["films"][4][3],
+                4: links["films"][4][4],
+                5: links["films"][4][5],
+            },
+            5:{
+                1: links["films"][5][1],
+                2: links["films"][5][2],
+                3: links["films"][5][3],
+                4: links["films"][5][4],
+                5: links["films"][5][5],
+            },
+        },
+        3: {
+            1: {
+                1: links["films"][6][1],
+                2: links["films"][6][2],
+                3: links["films"][6][3],
+                4: links["films"][6][4],
+                5: links["films"][6][5],
+            },
+            2: {
+                1: links["films"][6][1],
+                2: links["films"][6][2],
+                3: links["films"][6][3],
+                4: links["films"][6][4],
+                5: links["films"][6][5],
+            },
+            3: {
+                1: links["films"][6][1],
+                2: links["films"][6][2],
+                3: links["films"][6][3],
+                4: links["films"][6][4],
+                5: links["films"][6][5],
+            },
+            4: {
+                1: links["films"][4][1],
+                2: links["films"][4][2],
+                3: links["films"][4][3],
+                4: links["films"][4][4],
+                5: links["films"][4][5],
+            },
+            5:{
+                1: links["films"][5][1],
+                2: links["films"][5][2],
+                3: links["films"][5][3],
+                4: links["films"][5][4],
+                5: links["films"][5][5],
+            },
+        },
+    },
+    2: {
+        1: {
+            1: {
+                1: links["films"][1][1],
+                2: links["films"][1][2],
+                3: links["films"][1][3],
+                4: links["films"][1][4],
+                5: links["films"][1][5],
+            },
+            2: {
+                1: links["films"][2][1],
+                2: links["films"][2][2],
+                3: links["films"][2][3],
+                4: links["films"][2][4],
+                5: links["films"][2][5],
+            },
+            3: {
+                1: links["films"][3][1],
+                2: links["films"][3][2],
+                3: links["films"][3][3],
+                4: links["films"][3][4],
+                5: links["films"][3][5],
+            },
+            4: {
+                1: links["films"][4][1],
+                2: links["films"][4][2],
+                3: links["films"][4][3],
+                4: links["films"][4][4],
+                5: links["films"][4][5],
+            },
+            5:{
+                1: links["films"][5][1],
+                2: links["films"][5][2],
+                3: links["films"][5][3],
+                4: links["films"][5][4],
+                5: links["films"][5][5],
+            },
+        },
+        2: {
+            1: {
+                1: links["films"][6][1],
+                2: links["films"][6][2],
+                3: links["films"][6][3],
+                4: links["films"][6][4],
+                5: links["films"][6][5],
+            },
+            2: {
+                1: links["films"][2][1],
+                2: links["films"][2][2],
+                3: links["films"][2][3],
+                4: links["films"][2][4],
+                5: links["films"][2][5],
+            },
+            3: {
+                1: links["films"][3][1],
+                2: links["films"][3][2],
+                3: links["films"][3][3],
+                4: links["films"][3][4],
+                5: links["films"][3][5],
+            },
+            4: {
+                1: links["films"][4][1],
+                2: links["films"][4][2],
+                3: links["films"][4][3],
+                4: links["films"][4][4],
+                5: links["films"][4][5],
+            },
+            5:{
+                1: links["films"][5][1],
+                2: links["films"][5][2],
+                3: links["films"][5][3],
+                4: links["films"][5][4],
+                5: links["films"][5][5],
+            },
+        },
+        3: {
+            1: {
+                1: links["films"][6][1],
+                2: links["films"][6][2],
+                3: links["films"][6][3],
+                4: links["films"][6][4],
+                5: links["films"][6][5],
+            },
+            2: {
+                1: links["films"][6][1],
+                2: links["films"][6][2],
+                3: links["films"][6][3],
+                4: links["films"][6][4],
+                5: links["films"][6][5],
+            },
+            3: {
+                1: links["films"][6][1],
+                2: links["films"][6][2],
+                3: links["films"][6][3],
+                4: links["films"][6][4],
+                5: links["films"][6][5],
+            },
+            4: {
+                1: links["films"][4][1],
+                2: links["films"][4][2],
+                3: links["films"][4][3],
+                4: links["films"][4][4],
+                5: links["films"][4][5],
+            },
+            5:{
+                1: links["films"][5][1],
+                2: links["films"][5][2],
+                3: links["films"][5][3],
+                4: links["films"][5][4],
+                5: links["films"][5][5],
+            },
+        },
+    }
+}
+
+
+film_genre = links["film_genre"]
+film_year = links["film_year"]
+your_mood = links["your_mood"]
+your_years_old = links["your_years_old"]
+choices = []
+print("Выберите возвраст")
+for i in your_years_old:
+    print(str(i) + " " + your_years_old[i])
+choise: int = input()
+choices.append(choise)
+
+print("Выберите настроение")
+for i in your_mood:
+    print(str(i) + " " + your_mood[i])
+choise: int = input()
+choices.append(choise)
+
+print("Выберите жанр")
+for i in film_genre:
+    print(str(i) + " " + film_genre[i])
+choise: int = input()
+choices.append(choise)
+
+print("Выберите год выхода")
+for i in film_year:
+    print(str(i) + " " + film_year[i])
+choise: int = input()
+choices.append(choise)
+
+print(choices)
+print(choises
+      [int(choices[0])]
+      [int(choices[1])]
+      [int(choices[2])]
+      [int(choices[3])]
+      )
